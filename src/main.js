0fca59460b8f4f3fddd96fa16f53a5fe64df9121
WJZ_P <110795301+WJZ-P@users.noreply.github.com>
2024-10-25 10:15:57 +0800

    浅浅写写代码吧

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 // 运行在 Electron 主进程 下的插件入口
-
+const {pluginLog} = require("./utils/logUtils");
+const grAPI = window.grab_red_pack
 // 创建窗口时触发
 module.exports.onBrowserWindowCreated = window => {
     // window 为 Electron 的 BrowserWindow 实例
+
+    pluginLog("尝试监听onRecvMsg")
+    //尝试监听onRecvMsg
+    grAPI.subscribeEvent("nodeIKernelMsgListener/onRecvMsg", (payload) => {
+        console.log(payload)
+    })
 }
--- src/preload.js
+++ src/preload.js
@@ -1,7 +1,70 @@
 // Electron 主进程 与 渲染进程 交互的桥梁
-const { contextBridge } = require("electron");
+const {contextBridge, ipcRenderer} = require("electron");
+
+let {webContentsId} = ipcRenderer.sendSync('___!boot');
+if (!webContentsId) {
+    webContentsId = 2;
+}
 
 // 在window对象下导出只读对象
 contextBridge.exposeInMainWorld("grab_red_pack", {
-
+    invokeNative: (eventName, cmdName, registered, ...args) => invokeNative(eventName, cmdName, registered, ...args),
+    subscribeEvent: (cmdName, handler) => subscribeEvent(cmdName, handler),
+    unsubscribeEvent: (handler) => unsubscribeEvent(handler)
 });
+
+
+/**
+ * 调用一个qq底层函数，并返回函数返回值。
+ *
+ * @param { String } eventName 函数事件名。
+ * @param { String } cmdName 函数名。
+ * @param { Boolean } registered 函数是否为一个注册事件函数。
+ * @param  { ...any } args 函数参数。
+ * @returns { Promise<any> } 函数返回值。
+ */
+function invokeNative(eventName, cmdName, registered, ...args) {
+    return new Promise(resolve => {
+        const callbackId = crypto.randomUUID();
+        const callback = (event, ...args) => {
+            if (args?.[0]?.callbackId == callbackId) {
+                ipcRenderer.off(`IPC_DOWN_${webContentsId}`, callback);
+                resolve(args[1]);
+            }
+        };
+        ipcRenderer.on(`IPC_DOWN_${webContentsId}`, callback);
+        ipcRenderer.send(`IPC_UP_${webContentsId}`, {
+            type: 'request',
+            callbackId,
+            eventName: `${eventName}-${webContentsId}${registered ? '-register' : ''}`
+        }, [cmdName, ...args]);
+    });
+}
+
+/**
+ * 为qq底层事件 `cmdName` 添加 `handler` 处理器。
+ *
+ * @param { String } cmdName 事件名称。
+ * @param { Function } handler 事件处理器。
+ * @returns { Function } 新的处理器。
+ */
+function subscribeEvent(cmdName, handler) {
+    const listener = (event, ...args) => {
+        if (args?.[1]?.[0]?.cmdName == cmdName) {
+            handler(args[1][0].payload);
+        }
+    };
+    ipcRenderer.on(`IPC_DOWN_${webContentsId}`, listener);
+    return listener;
+}
+
+/**
+ * 移除qq底层事件的 `handler` 处理器。
+ *
+ * 请注意，`handler` 并不是传入 `subscribeEvent` 的处理器，而是其返回的新处理器。
+ *
+ * @param { Function } handler 事件处理器。
+ */
+function unsubscribeEvent(handler) {
+    ipcRenderer.off(`IPC_DOWN_${webContentsId}`, handler);
+}
